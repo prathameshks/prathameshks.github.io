c5b9421369e3d6c264ee5ebc55a5a1c599425d4e
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2023-12-17 14:00:27 +0530

    working home page

--- js/myjs.js
+++
@@ -1,8 +0,0 @@
-$.get("/header.html", function (result) {
-    html = result;
-    document.getElementById("header").insertAdjacentHTML("afterend", html);
-});
-$.get("/footer.html", function (result) {
-    html = result;
-    document.getElementById("footer").insertAdjacentHTML("afterend", html);
-});
